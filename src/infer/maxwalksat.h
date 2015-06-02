7188008203fc9db0fd3524518059fb4ea376535a
Kevin R Crain <craink@corn23.stanford.edu>
2015-06-02 16:26:09 -0700

    added loop to mcsat for reading in constraints, removed from maxwalksat

--- src/infer/maxwalksat.h
+++ src/infer/maxwalksat.h
@@ -192,35 +192,7 @@ class MaxWalkSat : public SAT
     long double lowCost = LDBL_MAX;
     //MLN *origMLN = state_->mln_;
     
-    ifstream parityConstraintFile("/afs/.ir.stanford.edu/users/r/a/raine/323/alchemy-2/src/infer/clauses.txt");
-    //parityConstraintFile.open("clauses.txt");
-    string clausestr;
-
-    state_->printNetwork(cout);
-    Array<GroundClause *> newClauses;
-    if (parityConstraintFile.is_open())
-    {
-      
-      while (getline(parityConstraintFile, clausestr) )
-      {
-        cout << clausestr << endl;
-        stringstream ss(clausestr);
-        string token;
-        Array<int> predIndices;
-        while(getline(ss, token, ' ')) {
-          cout << token << endl;
-          int varNum = atoi(token.c_str());
-          predIndices.append(varNum);
-        }
-        GroundClause *newClause = state_->getGroundClauseFromIndices(predIndices);
-        newClauses.append(newClause);
-      }
-      parityConstraintFile.close();
-      state_->addNewClauses(1, newClauses);
-      state_->printNetwork(cout);
-    } else {
-      cout << "unable to open file!" << endl;
-    }
+   
       /* If keeping track of true clause groundings, then init to zero
     if (trackClauseTrueCnts_)
       resetCnts();
